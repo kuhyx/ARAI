69a095fdaf981c143d905bbca673e8b95b5b2172
Mateusz Szpyruk <mateusz.szpyruk@gmail.com>
2024-03-09 18:07:10 +0100

    Create code that responds to clients requests

--- Backend_correct/app.py
+++ Backend_correct/app.py
@@ -1,18 +1,13 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/", methods=['POST'])
+@app.route("/", methods=['GET'])
 def recommended_mediators():
     data = request.get_json()
 
-    request_type = data.get('request_type', {}).get('request_type')
-    request_data = data.get('request_data', {}).get('experts_called')
-    generic_input = data.get('generic_input', {}).get('generic_input')
-    trial_cost = data.get('trial_cost', {}).get('trial_cost')
-    localization = data.get('localization', {}).get('localization')
-    experts_called = data.get('experts_called', {}).get('experts_called')
-    witnesses_called = data.get('witnesses_called', {}).get('witnesses_called')
+    request_type = data.get('request_data', {})
+    print(request_type)
 
     top_5 = {
         "response_type": "recommended_mediators",
@@ -22,21 +17,21 @@ def recommended_mediators():
             }, [{
             "name": "Mateusz Szpyruk",
             "specialization": "Prawo podatkowe",
-            "localization": localization,
+            "location": "Katowice",
             "ai_rating": 99,
             "user_rating": 99,
             "number_of_opinions": 5
             }, {
             "name": "Jan Kowalski",
             "specialization": "Prawo pracy",
-            "localization": localization,
+            "location": "Katowice",
             "ai_rating": 90,
             "user_rating": 99,
             "number_of_opinions": 5
             }, {
             "name": "Jan Kowalski",
             "specialization": "Prawo pracy",
-            "localization": localization,
+            "location": "Katowice",
             "ai_rating": 90,
             "user_rating": 99,
             "number_of_opinions": 5
